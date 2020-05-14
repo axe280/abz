a0b3212b83f8124d1d17393c6b567655bdf9dd03
Alexey Zibarev <axeworkmail@gmail.com>
2020-05-14 17:15:09 +0300

    base and header

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div 
+    id="app"
+    @click.self="menuIsOpen = false"
+    :class="menuIsOpen ? 'menu_opened' : ''"
+  >
+    <div class="site-wrapper">
+
+      <!-- header -->
+      <header-page
+        @menuOpen="menuIsOpen = true"
+      ></header-page>
+      <!-- end header -->
+
+      <main class="main">
+        
+        
+        <div class="container">
+          <button class="btn btn_flat">button</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem maiores voluptate harum ut nulla officia odio officiis odit quasi quis illo sit ullam laboriosam, incidunt ipsam repudiandae sunt praesentium tempora.</p>
+        </div>
+        
+      </main>
+    </div>
+
+
+    <!-- footer -->
+    <footer class="footer" id="footer">
+      <div class="container">
+        <span class="copy">Design by Axe</span>
+      </div>
+    </footer>
+    <!-- end footer -->
+  </div>
+</template>
+
+<script>
+import HeaderPage from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderPage
+  },
+
+  data() {
+    return {
+      menuIsOpen: false
+    }
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,93 @@
+<template>
+  <header class="header" id="header">
+    <div class="container">
+
+      <div class="logo">
+        <a href="/">
+          <div class="logo__image">
+            <img src="../assets/img/logo.svg" alt="logo"/>
+          </div>
+          <div class="logo__name">testtask</div>
+        </a>
+      </div>
+
+      <div class="head-menu">
+        <div 
+          @click="$emit('menuOpen')"
+          class="burger-menu"
+        >
+          <div class="burger-menu-box">
+            <div class="burger-menu-inner"></div>
+          </div>
+        </div>
+
+        <div class="menu-wrapper-fixed">
+          <div class="menu-open-wrapper">
+            <nav class="header-nav">
+              <ul class="menu">
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">About me</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link active" href="#">Relationships</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Requirements</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Users</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Sign Up</a>
+                </li>
+              </ul>
+
+              <ul class="menu menu_devices">
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">How it works</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Partnership</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Help</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Leave testimonial</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Contact us</a>
+                </li>
+              </ul>
+
+              <ul class="menu menu_devices">
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Articles</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Our news</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Testimonials</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Licenses</a>
+                </li>
+                <li class="menu-item">
+                  <a class="menu-item__link" href="#">Privacy Policy</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
