1e5ec9fac359616856b540a8919e718b80de5342
Alexey Zibarev <axeworkmail@gmail.com>
2020-05-29 14:02:10 +0300

    banner adaptive image

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -1,5 +1,32 @@
 <template>
   <section class="main-banner">
+    <div class="banner-img">
+      <picture>
+        <source
+          media="(max-width: 767px)"
+          srcset="
+            ../assets/img/banner-photo_mob_768.jpg 768w,
+            ../assets/img/banner-photo_mob_1366.jpg 1366w"
+        >
+        <source
+          media="(min-width: 768px) and (max-width: 991px)"
+          srcset="
+            ../assets/img/banner-photo_mob_768.jpg 768w,
+            ../assets/img/banner-photo_mob_1366.jpg 1366w,
+            ../assets/img/banner-photo_mob_1982.jpg 1982w"
+        >
+        <img
+          srcset="
+            ../assets/img/banner-photo_992.jpg 992w,
+            ../assets/img/banner-photo_1807.jpg 1807w,
+            ../assets/img/banner-photo_2470.jpg 2470w,
+            ../assets/img/banner-photo_3786.jpg 3786w,
+            ../assets/img/banner-photo_5000.jpg 5000w"
+          src="../assets/img/banner-photo_5000.jpg"
+          alt=""
+        >
+      </picture>
+    </div>
     <div class="container">
       <h1>Test assignment for Frontend Developer position</h1>
       <p>
